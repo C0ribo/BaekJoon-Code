454ca2c6a30f09dfc230e65ad97ae70a2810775f
EunKyo Chae <dmsry1022@gmail.com>
2023-09-12 20:07:27 +0900

    브론즈 5 문제풀이

--- src/브론즈/Baekjoon11720.java
+++ src/브론즈/Baekjoon11720.java
@@ -3,7 +3,6 @@ package 브론즈;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Baekjoon11720 {
     public static void main(String[] args) throws IOException {
