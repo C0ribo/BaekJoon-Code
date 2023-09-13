71c624bc59be8d20f04fb0ff41a9c2b744d7ec80
EunKyo Chae <dmsry1022@gmail.com>
2023-09-13 19:07:21 +0900

    브론즈 5 문제풀이

---
+++ src/브론즈/Baekjoon11654.java
@@ -0,0 +1,25 @@
+package 브론즈;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon11654 {
+    /*
+    * 브론즈 5 - 11654
+    * 소문자, 대문자 , 숫자 0 - 9 중 하나 입력했을 때, 이스키 코드 값을 출력한다.
+    * 내 생각 : String으로 받아서 charAt로 뽑아낸다.
+    * */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        //String str = br.readLine();
+        //char ch = str.charAt(0);
+        int ch2 = br.readLine().charAt(0);
+
+        sb.append(ch2);
+        System.out.println(sb);
+
+    }
+}
