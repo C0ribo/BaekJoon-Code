db1d8e688a996c6473b1823145e4402fddff1695
EunKyo Chae <dmsry1022@gmail.com>
2023-09-14 18:25:38 +0900

    브론즈 4 문제풀이

---
+++ src/브론즈/Baekjoon3046.java
@@ -0,0 +1,27 @@
+package 브론즈;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon3046 {
+    /*
+     * 브론즈 4 - 3046
+     *
+     * R1 R2 S 가 있는데 결론은 R2 를 구하는 것이다.
+     * */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+
+        int r1 = Integer.parseInt(st.nextToken());
+        int avgS = Integer.parseInt(st.nextToken());
+
+        int r2 = (2 * avgS) - r1;
+        sb.append(r2);
+        System.out.println(sb);
+    }
+}
---
+++ src/브론즈/Baekjoon5543.java
@@ -0,0 +1,32 @@
+package 브론즈;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon5543 {
+    /*
+    * 브론즈 4 - 5543
+    *
+    * 제일 싼 버거 + 제일 싼 음료 - 50 = 출력
+    * */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int highBurger = Integer.parseInt(br.readLine()); // 상덕 버거
+        int middleBurger = Integer.parseInt(br.readLine()); // 중덕 버거
+        int lowBurger = Integer.parseInt(br.readLine()); // 하덕 버거
+
+        int cola = Integer.parseInt(br.readLine()); // 콜라
+        int cider = Integer.parseInt(br.readLine()); // 사이다
+
+        int lowPriceBurger = Math.min(highBurger, Math.min(middleBurger, lowBurger));
+        int lowPriceDrink = Math.min(cola, cider);
+
+        int setBurger = (lowPriceBurger + lowPriceDrink) - 50;
+
+        sb.append(setBurger);
+        System.out.println(sb);
+    }
+}
