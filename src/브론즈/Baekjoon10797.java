61e2b599632a520096c57b2e26c4529615f8dca1
EunKyo Chae <dmsry1022@gmail.com>
2023-09-16 12:28:03 +0900

    브론즈 4 문제풀이

--- src/브론즈/Baekjoon10797.java
+++ src/브론즈/Baekjoon10797.java
@@ -11,44 +11,47 @@ public class Baekjoon10797 {
     *
     * 날짜 일의 자리를 입력했을 때 차량 5대 중에 그 숫자와 동일한 수가 몇개인지 찾으시오
     * */
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringBuilder sb = new StringBuilder();
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-//
-//        int day = Integer.parseInt(br.readLine()); // 일의 날짜 입력
-//        int cnt = 0;
-//
-//        for(int i = 0; i< 5; i++){
-//            int carNum = Integer.parseInt(st.nextToken());
-//            if(day == carNum){ // day 와 arrCar 이 같으면
-//                // 몇개인지 알아야한다.
-//                cnt++;
-//            }
-//            sb.append(cnt);
-//        }
-//        //sb.append(cnt);
-//        System.out.println(sb);
-//
-//    }
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;// = new StringTokenizer(br.readLine());
 
-        int day = Integer.parseInt(br.readLine());
-        int[] arr = new int[5];
-        int count = 0;
+        int day = Integer.parseInt(br.readLine()); // 일의 날짜 입력
+        String str = br.readLine();
+        st = new StringTokenizer(br.readLine());
+        int cnt = 0;
 
-        for(int i = 0; i<arr.length; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-
-            if(arr[i] == day)
-                count++;
-            sb.append(count);
+        for(int i = 0; i< 5; i++){
+            int carNum = Integer.parseInt(st.nextToken());
+            if(day == carNum){ // day 와 arrCar 이 같으면
+                // 몇개인지 알아야한다.
+                cnt++;
+            }
+            sb.append(cnt);
         }
+        //sb.append(cnt);
         System.out.println(sb);
+
     }
+
+//    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        StringTokenizer st;
+//
+//        int day = Integer.parseInt(br.readLine());
+//        st = new StringTokenizer(br.readLine());
+//        int[] arr = new int[5];
+//        int count = 0;
+//
+//        for(int i = 0; i<arr.length; i++){
+//            arr[i] = Integer.parseInt(st.nextToken());
+//
+//            if(arr[i] == day)
+//                count++;
+//            sb.append(count);
+//        }
+//        System.out.println(sb);
+//    }
 }
 
