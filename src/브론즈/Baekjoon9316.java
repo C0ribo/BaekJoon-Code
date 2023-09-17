3383222a0b5bc9b8d7da6168b6cf912e0cc7d55b
EunKyo Chae <dmsry1022@gmail.com>
2023-09-17 17:31:48 +0900

    브론즈 4 문제풀이

---
+++ src/브론즈/Baekjoon9316.java
@@ -0,0 +1,24 @@
+package 브론즈;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon9316 {
+    /*
+    * 브론즈 4 - Hello Judge
+    *
+    * n 반복개수
+    *
+    * */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i =1; i<=n; i++){
+            sb.append("Hello World, Judge").append(" ").append(i).append("!").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
