f5fcd2b4ce3bcf6db00ac7e4ba30b973b950aa33
EunKyo Chae <dmsry1022@gmail.com>
2023-10-08 23:42:50 +0900

    백준 알고리즘 브론즈 2 1152 StringTokenizer

--- src/브론즈/Baekjoon1152.java
+++ src/브론즈/Baekjoon1152.java
@@ -3,11 +3,13 @@ package 브론즈;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Baekjoon1152 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         String str = br.readLine();
     }
