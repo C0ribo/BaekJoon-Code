330c05e77db03e21d3a71616ef48fdebc0b82860
EunKyo Chae <dmsry1022@gmail.com>
2023-10-09 22:37:10 +0900

    백준 알고리즘 브론즈 2 1152 st fix

--- src/브론즈/Baekjoon1152.java
+++ src/브론즈/Baekjoon1152.java
@@ -9,8 +9,16 @@ public class Baekjoon1152 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        String str = br.readLine();
+        String str = st.nextToken().trim(); // String 문자열 입력, 공백제거
+        st = new StringTokenizer(br.readLine(), str); // 단어는 공백하나로 구분
+
+        // countTokens으로 토큰의 개수를 출력한다.(int형) -> countTokens = StringTokenizer에서만 쓸 수 있나봄
+        //int strNum = Integer.valueOf(str); // string -> int
+        int count = st.countTokens();
+        
+        sb.append(count).append(" ");
+        System.out.println(sb);
     }
 }
